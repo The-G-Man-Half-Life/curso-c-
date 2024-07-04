8f147042f993e170fb8a350365a30c0590ae47d2
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-07-04 07:10:43 -0500

    ciclos

---
+++ terceraClase/Program.cs
@@ -0,0 +1,30 @@
+﻿// ciclo for 
+//crear el contador/decir cuantas vueltas realizara/ realizar el incrementador numerico
+
+for(int i = 0; i<=10;i++)
+{
+    Console.WriteLine("Hola mundo");
+}
+
+//ciclo foreach
+
+string[] nombres = {"juan","pedro","ana"};
+foreach(string nombre in nombres)
+{
+    Console.WriteLine($"hola");
+}
+
+//ciclo while
+
+bool flag = true;
+
+while (flag)
+{
+    Console.WriteLine("Hola tonotos");
+    Console.Write("Quieres repetir la palabra");
+    string? respuesta = Console.ReadLine().ToLower();
+    if(respuesta =="no")
+    {
+        flag = false;
+    }
+}
