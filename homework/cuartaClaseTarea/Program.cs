e40f832588c8e09bc1a517414cccf2c3b4451db3
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-07-08 08:56:56 -0500

    chamba

---
+++ homework/cuartaClaseTarea/Program.cs
@@ -0,0 +1,73 @@
+﻿//1
+
+List<int> numbers1To5 = new List<int>();
+
+for (int i = 1; i <= 5; i++)
+{
+    numbers1To5.Add(i);
+}
+// foreach (var item in numbers1To5)
+// {
+//     Console.WriteLine(item);
+// }
+
+//2
+
+numbers1To5.Add(6);
+
+int ubication3 = numbers1To5.IndexOf(3);
+
+numbers1To5.RemoveAt(ubication3);
+
+// foreach (var item in numbers1To5)
+// {
+//     Console.WriteLine(item);
+// }
+
+//3
+
+// Console.WriteLine(numbers1To5[1]);
+
+//4
+
+bool existencia5 = numbers1To5.Contains(5);
+
+// Console.WriteLine("¿El numero existe en la lista: " + existencia5);
+
+//5
+
+// foreach (var number in numbers1To5)
+// {
+//     Console.WriteLine(number);
+// }
+
+//6
+
+
+
+//7
+//8
+//9
+//10
+//11
+//12
+//13
+//14
+//15
+//16
+//17
+//18
+//19
+//20
+
+// List<int> numbers1To5 = new List<int>();
+
+// for (int i = 1; i <= 5; i++)
+// {
+//     numbers1To5.Add(i);
+// }
+// foreach (var item in numbers1To5)
+// {
+//     Console.WriteLine(item);
+// }
+
