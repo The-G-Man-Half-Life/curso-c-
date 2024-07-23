dfca28f69ae0c07663f678192befabc900eceb00
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-07-23 08:51:46 -0500

    feat: added content

---
+++ Clases/ClaseNovena/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClaseNOvena.Models;
+
+void separator()
+{
+    Console.WriteLine(@" 
+***************************************
+");
+}
+
+var mainPet = new Animal("pepe","macho","estrella",120);
+mainPet.ToTalk();
+mainPet.ToMove();
+separator();
+var firstPet = new Dog();
+firstPet.ToTalk();
+firstPet.ToMove();
+separator();
+var secondPet = new Cat();
+secondPet.ToTalk();
+secondPet.ToMove();
+separator();
+var thirdPet = new Fish();
+thirdPet.ToTalk();
+thirdPet.ToMove();
