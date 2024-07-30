9d8c3609e599e36cffc3930e4370f4fbc8803aa0
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-07-30 08:57:11 -0500

    feat: created a crud

--- homework/UndecimaClaseTarea/obj/Debug/net8.0/UndecimaClaseTarea.AssemblyInfo.cs
+++ homework/UndecimaClaseTarea/obj/Debug/net8.0/UndecimaClaseTarea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UndecimaClaseTarea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30b915757182e1d650792563e438e7bde16d765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02db9425262221cfac68d742bf302bf35a23f59c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UndecimaClaseTarea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UndecimaClaseTarea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
