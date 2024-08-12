fd7f7af7f65fb342f54fa577d868824ea2405c1a
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-08-12 08:52:01 -0500

    feat: created a .net project

---
+++ Clases/AplicacionConsola/Program.cs
@@ -0,0 +1,10 @@
+﻿using Humanizer;
+
+string nombre = "mateo";
+string apellido = "montoya ospina";
+int sueldo = 300000000;
+Console.WriteLine($"Hola {nombre} {apellido}");
+Console.WriteLine($"");
+
+Console.WriteLine($"Tu sueldo actual es: {sueldo}");
+Console.WriteLine($"Osea ganas {sueldo.ToWords()} pesos colombianos");
