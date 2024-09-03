6616e419ee42e5a27bebdf07b0cbc27e4013eb69
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-09-03 06:55:12 -0500

    feat: there were added some validations to the owner model

--- Clases/TreceavaClase/Models/Owner.cs
+++ Clases/TreceavaClase/Models/Owner.cs
@@ -5,10 +5,24 @@ public class Owner
 {
     [Key]
     public int Id { get; set; }
+
+    [MinLength(5,ErrorMessage = "The full name must be at least {5} character")]
+    [MaxLength(125,ErrorMessage = "The name field must be at most {125} characters")]
     public required string Name { get; set; }
+    
     public required string Apellido { get; set; }
     public required string IdentificationNumber { get; set; }
+    
+    [MaxLength(100,ErrorMessage ="The profile photo must be at most 100 characters")]
+    [Url(ErrorMessage = "The profile photo must be a valid url")]
+    public required string ProfilePhoto {get; set;}
     public required string Address { get; set; }
+    
+    [MinLength(5,ErrorMessage = "The phone field must have at least 5 characters")]
+    [MaxLength(255,ErrorMessage = "The phone field must have at most 255 characters ")]
+    [Phone(ErrorMessage = "The phone format is not valid, ")]
     public required string Phone { get; set; }
+
+    [EmailAddress(ErrorMessage = "This email field is using an invalid email format")]
     public required string Email { get; set; }
 }
